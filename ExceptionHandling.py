9a6a5cbe73f29dd2dbcd4d204d95282a711bd443
Binayak Bhattarai <82257060+MrBinayakB@users.noreply.github.com>
2022-07-18 08:51:52 +0545

    Create ExceptionHandling.py

---
+++ ExceptionHandling.py
@@ -0,0 +1,7 @@
+num=int(input("Enter numerator:"))
+den=int(input("Enter denumerator:"))
+try:
+    print(num//den)
+except:
+    print("""Basic Maths pani aaudaina kya bhai??
+    Zero le divide hudaina""")
