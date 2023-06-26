66f545f0629ad2c4bf92a74fe99dedc02dd4f909
MrBinayakB <b.binayak07@gmail.com>
2023-06-26 22:19:50 +0545

    Search Insert Position

--- LeetCode.py
+++ LeetCode.py
@@ -36,3 +36,17 @@ class Solution(object):
             else:
                 target_pos+=1
         return target_pos
+    
+#longest Common Prefix
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if len(strs)==0:
+            return""
+
+        base=strs[0]
+        for i in range(len(base)):
+            for word in strs[1:]:
+                if i == len(word) or word[i]!= base[i]:
+                    return base[0:i]
+
+        return base
